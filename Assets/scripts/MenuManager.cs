9fd21beca73862692580a4d6f1985f794ac8befe
Emily Roderick <Emily Roderick>
2018-03-01 17:30:08 +0000

    Changeset: 39 Possible success of sounds played when bots are moved - I have no game to test this on. ER

--- Assets/scripts/MenuManager.cs
+++ Assets/scripts/MenuManager.cs
@@ -5,40 +5,41 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 //using UnityEngine.Timeline;
 
-public class MenuManager : MonoBehaviour
+public class MenuManager : AbstractManager
 {
     //float Timer = 0.0F;
     int FRAME_NEXT_SOUND = 0;
-    public AudioClip Menu_Sound;
+    public AudioClip Menu_Sound, Bot1_Move_Sound, Bot2_Move_Sound, Bot3_Move_Sound;
     //int Time.frameCount = 0;
     public static int frameCount;
     //public int Time.frameCount { get; }
     public int RandomNumber { get; private set; }
+    bool BOT_ACTIVATED = false;
 
     // Use this for initialization
     // time related
 
     public void FreePlay()
     {
-		SceneManager.LoadScene("FreePlay", LoadSceneMode.Single);
+        SceneManager.LoadScene("FreePlay", LoadSceneMode.Single);
     }
-    
+
     public void TouchTouchRevolution()
     {
         SceneManager.LoadScene("TouchTouchRevolution", LoadSceneMode.Single);
     }
 
     void Update()
-        {
+    {
 
-            if (Time.frameCount >= FRAME_NEXT_SOUND)
-            {
+        if (Time.frameCount >= FRAME_NEXT_SOUND)
+        {
             playSound();
             FRAME_NEXT_SOUND = Time.frameCount + Random.Range(300, 1500);
-            }
-
         }
-       
+
+    }
+
     void playSound()
     {
         //gameObject.transform.Find("RandomSound").GetComponent<AudioSource>().clip = Menu_Sound as AudioClip;
@@ -48,9 +49,24 @@ public class MenuManager : MonoBehaviour
         Debug.Log("Play Me!");
     }
 
-     void BotMovement()
-     {
-         //this function will play a random sound if one of the bots are moved
-     }
-}
+    public override void BoxOneStartMoving(double speed)
+    {
+        gameObject.GetComponent<AudioSource>().clip = Bot1_Move_Sound as AudioClip;
+        gameObject.GetComponent<AudioSource>().Play();
+        Debug.Log("You've woken up Bot1!");
+    }
 
+    public override void BoxTwoStartMoving(double speed)
+    {
+        gameObject.GetComponent<AudioSource>().clip = Bot2_Move_Sound as AudioClip;
+        gameObject.GetComponent<AudioSource>().Play();
+        Debug.Log("You've woken up Bot2!");
+    }
+
+    public override void BoxThreeStartMoving(double speed)
+    {
+        gameObject.GetComponent<AudioSource>().clip = Bot3_Move_Sound as AudioClip;
+        gameObject.GetComponent<AudioSource>().Play();
+        Debug.Log("You've woken up Bot3!");
+    }
+}
