39c74fffaca1405badf906973de431144c711e67
phoenix perry <phoenix perry>
2018-03-15 22:53:25 +0000

    Changeset: 74 I kept my version of the code b/c I know it's working. it's the same I think so we're fine.

--- Assets/scripts/BaseBehavior/SpawnGame.cs
+++ Assets/scripts/BaseBehavior/SpawnGame.cs
@@ -2,7 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 //this script is critical for spawning all of the prefabs which implement all the core game data into the hierarcy when level starts
+
 public class SpawnGame : MonoBehaviour {
+    //this is to keep the game running in background mode
+    public bool debugLog = false;
+    public bool runInBackgroundValue = true; 
 
     // these are the game prefabs. find them in the prefab folder 
     public GameObject serialDataManager;
@@ -22,6 +26,7 @@ public class SpawnGame : MonoBehaviour {
 		touchManager = Instantiate (touchManager, new Vector3 (0.0f, 0.0f, 0.0f), Quaternion.identity) as GameObject;
 		serial = true;
 		switchControls (serial);
+        Application.runInBackground = true;
 	}
     //lets you switch between keyboard and serial data 
 	void switchControls(bool serial) {
@@ -40,6 +45,18 @@ public class SpawnGame : MonoBehaviour {
 			serial = !serial;
 			switchControls (serial);
 		}
-        if (Input.GetKeyDown(KeyCode.Escape)) Application.Quit(); 
+        if (Input.GetKeyDown(KeyCode.Escape)) Application.Quit();
+
+        if (debugLog) {
+            runInBackgroundValue = Application.runInBackground; 
+        }
+        if (!Application.runInBackground)
+        {
+            Application.runInBackground = true;
+            if (debugLog)
+            {
+                Debug.Log("Resetting appplcation.runInBackground mode to true at: " + Time.time);
+            }
+        }
 	}
 }
