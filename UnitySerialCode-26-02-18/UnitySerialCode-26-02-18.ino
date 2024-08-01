b8b02f8df52d787fc8149cd4bf2742abde33025e
Charlie Ann Page <me@charlieannpage.co.uk>
2024-08-01 09:00:36 +0100

    Motor change

--- UnitySerialCode-26-02-18/UnitySerialCode-26-02-18.ino
+++ UnitySerialCode-26-02-18/UnitySerialCode-26-02-18.ino
@@ -532,7 +532,7 @@ bool readOneLEDInstruction(String instruction) {
       }
       return true;
 }
-int MOTOR_BASE_VALUE = 10;
+int MOTOR_BASE_VALUE = 3;
 void ledFadeOn(int led, int parameter) {
   if (led == 1) {
     LED1_MODE = 1;
